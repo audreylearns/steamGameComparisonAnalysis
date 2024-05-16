7cec96a21096d7f8b02267d5c8f4f70995b13484
audreylearns <audreyduzon.cda@gmail.com>
2024-05-16 12:53:13 -0400

    query rev_summary, localization, nested dict handle

--- model/pipeline.py
+++ model/pipeline.py
@@ -1,3 +1,37 @@
-#ingestion
+#ingestion ?
 import pandas as pd
+import utils.fetch as fetch
+
+
+
+# display all columns
+pd.set_option('display.max_columns', None) 
+
+# 1 obj, sereis
+# res = fetch.get_game_details('774171')
+# gamedf = pd.DataFrame(res["price_overview"])
+# gamedf.head()
+
+# retrieve data, must be from control
+# data = fetch.get_rev('774171', 300)
+
+# handle nested dictionary author
+# rtn row q author.column
+# df = pd.json_normalize(data,max_level=1)
+
+# ----------LOCALIZATION FOR EXPLORATION----------
+# OPT1: convert pandas to csv, no indexes 
+# df.to_csv('out.csv', index=False) 
+# READ csv to panda
+# pd.read_csv('out.csv')  
+
+
+# OPT 2: convert panda to paqruet
+# df.to_parquet('sample.parquet')
+# READ parquet to panda
+df = pd.read_parquet('sample.parquet') 
+
+
+
+
 
--- model/utils/fetch.py
+++ model/utils/fetch.py
@@ -2,28 +2,17 @@ msg = "test"
 print(msg)
 
 import requests 
-from bs4 import BeautifulSoup 
+# from bs4 import BeautifulSoup 
 
-# url = "https://store.steampowered.com/appreviews/413150?json=1"
-url = "https://store.steampowered.com/"
 
-# Making a GET request 
-# res = requests.get(url).json()
-  
-# check status code for response received 
-# success code - 200 
-# print(res) 
-  
-# print content of request 
-# print(res.content)
+#100k calls per day
+steam_url = "https://store.steampowered.com/"
+cheapshark_url = "https://www.cheapshark.com/api/1.0/games?"
+ 
 
-# print content of request PRETTY 
-# soup = BeautifulSoup(res.content, 'html.parser') 
-# print(soup.prettify()) 
-
-#app id = gameid, n = total num of revs to fetch
-#returns list of revs, dict struct
-#100 calls per day
+# app id = gameid, n = total num of revs to fetch
+# returns list of revs, dict struct
+# fetch max 100 reviews per req
 def get_rev(appid, n=100):
     reviews = []
     cursor = '*'
@@ -41,21 +30,20 @@ def get_rev(appid, n=100):
         params['num_per_page'] = min(100, n)
         n-=100
 
-        res = requests.get(url=url+'appreviews/'+appid, params=params).json()
+        res = requests.get(url=steam_url+'appreviews/'+appid, params=params).json()
         cursor = res['cursor']
         reviews += res['reviews']
         if len(res['reviews']) < 100: break
     return reviews
 
-# should return 300 revs of muse dash
-# max 100k calls per day
-# rev = get_rev('774171', 300)
+# quick test:
+# rev = get_rev('1222670', 300)
 # print(rev[299]["author"]["playtime_at_review"])
 # print(rev[0]["review"])
 
-# https://store.steampowered.com/api/appdetails?appids=774171
+
 def get_game_details(appid):
-    details = requests.get(url=url+'api/appdetails?appids='+appid).json()
+    details = requests.get(url=steam_url+'api/appdetails?appids='+appid).json()
     # details = requests.get("https://store.steampowered.com/api/appdetails?appids=774171").json()
     return details[appid]["data"]
 # price and deals details[appid]["data"]["price_overview"]
@@ -63,28 +51,18 @@ def get_game_details(appid):
 
 # game = get_game_details('774171')
 # print(game["name"])
-# print(game["detailed_description"])
+# print(game["short_description"])
 # detailed desc has tags
 
+
+# use the rtn to create a drop down list from keys
 def convert_currency(value, target='USD'):
     if (target != 'USD'):
         conv_rate = requests.get("https://open.er-api.com/v6/latest/USD").json()
         conv_rate = conv_rate["rates"][target]
         return round(value * float(conv_rate),2)
-
-# offer 12 currencies: 
-# AUD, AED, BRL, CAD, CHF, CNY, COP, CUP, DKK, EUR, HKD, JPY, KRW, MXN, PHP, VND, NZD
-#use the rtn to create a drop down list
-
+# quick test:
 # print(convert_currency(2.99, 'CNY'))
-
-#only calls to check if deals avail on steam, else empty
-# def game_deals(id):
-#     deal = requests.get("https://www.cheapshark.com/api/1.0/deals?storeID=1&onSale=1&steamAppID=" + id).json()
-#     return deal
-
-# print(game_deals('774171'))
-
 
 
 # maybe change to a local.json listing all the games?
@@ -93,16 +71,39 @@ def convert_currency(value, target='USD'):
 
 
 # return list, get user input to get the specific game
-# imp "external" for title display + "thumb" prev img
+# imp "external" for title display + "thumb" prev img, "gameid" for to grab steamappid
+# return null if game not avail on steam
 def game_search(title):
-    res = requests.get("https://www.cheapshark.com/api/1.0/games?title=" + title).json() 
+    res = requests.get(url=cheapshark_url+"title=" + title).json() 
+    # check if game exist in steam, ie steamAppID != null, return empty
     return res
 
 
-#return steam appid from cheapshark gamelookup gameid, parse steam
+# return steam appid from cheapshark gamelookup gameid, parse steam
+# ensure game exist in steam db
 def get_steamID(gameID):
-    res = requests.get("https://www.cheapshark.com/api/1.0/games?id=" + str(gameID)).json()
+    res = requests.get(url=cheapshark_url+"id=" + str(gameID)).json()
     return res["info"]["steamAppID"]
 
+# combo call sample
 # game_search('sims')
 # get_game_details(get_steamID(63))
+
+# returns a dict of review summary: total, positive, score for steamappid
+def get_reviewSummary(appid): 
+    summary = {}
+    params = {
+        'json' : 1,
+        'filter' : 'all',
+        'language' : 'english',
+        'day_range' : 9223372036854775807,
+        'review_type' : 'all',
+        'purchase_type' : 'all'
+    }
+    res = requests.get(url=steam_url+'appreviews/'+appid, params=params).json()
+    summary["total"] = res["query_summary"]["total_reviews"]
+    summary["positive"] = res["query_summary"]["total_positive"]
+    summary["score"] = summary["positive"]/ summary["total"]*100
+    return summary
+
+# get_reviewSummary("1222670")
