1b064a2664cf321f167469017f721b0cc8e0430e
audreylearns <audreyduzon.cda@gmail.com>
2024-05-24 16:04:35 -0400

    Sentiment Analysis Model complete. Polarity percentage complete

--- model/pipeline.py
+++ model/pipeline.py
@@ -1,16 +1,13 @@
 #ingestion ?
 import pandas as pd
 import utils.fetch as fetch
-
+from transformers import AutoTokenizer,  AutoConfig
+from transformers import AutoModelForSequenceClassification
+from scipy.special import softmax
 
 
 # display all columns
 pd.set_option('display.max_columns', None) 
-
-# 1 obj, sereis
-# res = fetch.get_game_details('774171')
-# gamedf = pd.DataFrame(res["price_overview"])
-# gamedf.head()
 
 # retrieve data, must be from control
 # data = fetch.get_rev('774171', 300)
@@ -20,60 +17,20 @@ pd.set_option('display.max_columns', None)
 # df = pd.json_normalize(data,max_level=1)
 
 # ----------LOCALIZATION FOR EXPLORATION----------
-# OPT1: convert pandas to csv, no indexes 
-# df.to_csv('out.csv', index=False) 
-# READ csv to panda
-# pd.read_csv('out.csv')  
-
-
-# OPT 2: convert panda to paqruet
+# convert panda to paqruet
 # df.to_parquet('sample.parquet')
 # READ parquet to panda
-df_raw = pd.read_parquet('sample.parquet') 
-df = df_raw.copy()
-
-# drop tables
-df.drop(columns=['timestamp_created', 'timestamp_updated','language', 'recommendationid', 'hidden_in_steam_china', 'steam_china_location','author.steamid'],axis=1, inplace=True)
+# df_raw = pd.read_parquet('sample.parquet') 
+# df = df_raw.copy()
 
 
-# Tests:
-#17, 2, 37(special chars), 41, 74, 298, 124(jap chars), 278 (link, tags)
 
-
-# rm tags, [any]
-df['review'] = df['review'].replace('\[.*?\]', '', regex=True)
-# rm all non-ASCII values:
-df['review'] = df['review'].replace('[^\x00-\x7F]', '', regex=True)
-# rm raw escape chars
-df['review'] = df['review'].replace('[\\n\\t]', ' ', regex=True)
-# Negative look behind: rm all / unless its num/num
-df['review'] = df['review'].replace("(?<!\d)\/", '', regex=True)
-# look behind, replace duplicates
-df['review'] = df['review'].replace("(?<=[/,'!])[/,'!]", '', regex=True)
-#  remove any links embedded
-df['review'] = df['review'].replace("http\S*",' ', regex=True)
-# only 1 space allowed
-df['review'] = df['review'].replace("\s{2,}", ' ', regex=True)
-# leave only alpha, num, punc, space 
-df['review'] = df['review'].replace("[^0-9a-zA-Z.',!\s/]+",'', regex=True)
-
-
-# rm rows if it doesnt have minimum 2 alphabet characters
-df = df[df['review'].str.count(r'[a-zA-Z]') >= 2]
-
-# FILTER: create new columns for vote results
-df['total'] = df['votes_up'] + df['votes_funny']
-df['score'] = (df['votes_up'] + 1) / (df['total'] + 1)
-
-# Goal, filter all reviews less than 0.50
-df_filtered= df.loc[(df['score'] >= .5)]
-
-# Fn format:
-# Call after fetch completed, input the returned object)
+# data: json object from an API call
 # returns a dataframe of steam reviews, with added columns: score &
 def remove_noise(data):
     df = pd.json_normalize(data,max_level=1)
     df.drop(columns=['timestamp_created', 'timestamp_updated','language', 'recommendationid', 'hidden_in_steam_china', 'steam_china_location','author.steamid'],axis=1, inplace=True)
+    
     # rm tags, [any]
     df['review'] = df['review'].replace('\[.*?\]', '', regex=True)
     # rm all non-ASCII values:
@@ -97,7 +54,6 @@ def remove_noise(data):
     return df
 
 
-#Fn format: 
 # returns a dataframe of steam reviews scoring minimum 50% from based on votes
 # new columns added to dataframe: total & score
 def filterby_score(df):
@@ -105,46 +61,25 @@ def filterby_score(df):
     df['total'] = df['votes_up'] + df['votes_funny']
     df['score'] = (df['votes_up'] + 1) / (df['total'] + 1)
     # Goal, filter all reviews less than 0.50
-    df_filtered= df.loc[(df['score'] >= .5)]
+    df_filtered= df.loc[(df['score'] >= .5)].copy()
     return df_filtered
 
-# ----------ADD ONS----------
-# FILTER; drop down | Radio button: specify hours played at review [<10, <30, >30]
-
-# Display: Top highly scored comment (applying the filter above?)
-
-# Display: Median scored comment (applying the filter above?)
-
-# Display: Lowest scored comment (applying the filter above?)
 
 
 # ----------MODEL----------
-# Roberta Pretrained Model
-# likely move to a diff script
-from transformers import AutoTokenizer,  AutoConfig
-from transformers import AutoModelForSequenceClassification
-from scipy.special import softmax
-
-# MODEL = f"cardiffnlp/twitter-roberta-base-sentiment-latest"
-# tokenizer = AutoTokenizer.from_pretrained(MODEL)
-# model = AutoModelForSequenceClassification.from_pretrained(MODEL)
-
-
+# Roberta Pretrained Model from huggingface
 MODEL = f"cardiffnlp/twitter-roberta-base-sentiment-latest"
 tokenizer = AutoTokenizer.from_pretrained(MODEL)
 config = AutoConfig.from_pretrained(MODEL)
 model = AutoModelForSequenceClassification.from_pretrained(MODEL)
 
 
-# return the compound score for string input if score is True
+# if score is True, returns the compound score for string input 
 # categorical: -1 (neg), 0 (neu), 1(pos)
-# else returns the polarity scores dictionary for string input
-# simplicitY: pick highest score among the the three
-# caveat: if evenly scored pos & neg == neutral
+# if score is False, returns the polarity scores dictionary for string input
 def polarity_scores(string, score=True):
     # encoded_text = tokenizer(string, max_length=128, truncation=True, padding='max_length', return_tensors='pt')
     encoded_text = tokenizer(string, return_tensors='pt')
-    num_tokens = len(encoded_text)
     output = model(**encoded_text)
     scores = output[0][0].detach().numpy()
     scores = softmax(scores)
@@ -167,42 +102,59 @@ def polarity_scores(string, score=True):
                     return 1
     else:
         return scores_dict
-# test run
-# polarity_scores(df_filtered['review'].iloc[0])
-# df_filtered['review'].iloc[1]
 
-# Goal: Adds sentiment score column to dataframe
+# Adds sentiment score column to dataframe
 # Input: a dataframe column to run analysis
 def sentiment_score(dataframe):
-    # max token of 512
+    # model token max at 512
+    dataframe['rev_len'] = dataframe['review'].str.len()
+    # process reviews up to 2300 characters
+    dataframe = dataframe.loc[(dataframe['rev_len'] <= 2300)].copy()
     dataframe['sentiment'] = dataframe['review'].apply(polarity_scores)
-    # text too large
+
     return dataframe    
 # print(sentiment_score(df_filtered))
 
 
-# length of max str length, words (only 512 tokens)
-#new column to store string, filter out max length to be handled
-df['rev_len'] = df['review'].str.len()
-
-
-# Get max length of string in col....returns the iloc
-# iloc_maxlen= df.iloc[df['rev_len'].argmax()]
-
-# Sort vals by string length asc
-df.sort_values(by=['rev_len'])
-
-# process up to rev len of 5k...
-df_500 = df.loc[(df['rev_len'] <= 5000)]
-
-
-# iloc 179 for 1077, 164 for 4823, 133 for 3668, 258 for 2220, 266 for 5209
-# breaks @198 len >7k....has to be less than 5k? max 5k
-# get iloc of idx where length of col is __
-# sample loc curr @ 285 rows
-def getiloc(len):
-    index_location = df_500.loc[df_500['rev_len'] == 4823].index[0]
-    iloc_location = df_500.index.get_loc(index_location)
-    return iloc_location
+# returns a dictionary of the review dataframe polarity percentage
+# 1: positive, 0: neutral, -1: negative
+def polarity_percentage(df):
+    total_processed = len(df.index)
+    result = df['sentiment'].value_counts().to_dict()
+    result[1] = round((result[1]/total_processed) * 100)
+    result[0] = round((result[0]/total_processed) * 100)
+    result[-1] = round((result[-1]/total_processed) * 100)
+    return result
 
 print("test")
+
+
+# ----------MODEL----------
+# Clustering - Word cloud
+
+
+# ----------ADD ONS----------
+# FILTER; drop down | Radio button: specify hours played at review [<10, <30, >30]
+
+# df_2300 = df_2300.sort_values(by=['score'])
+# # Display: Top highly scored comment (applying the filter above?)
+# top = df_2300[(df_2300['score'] == 1) & (df_2300['total'] > 0)].head()
+# polarity_scores(top['review'].iloc[0], False)
+# polarity_scores(top['review'].iloc[0])
+# top['review'].iloc[0]
+# # Display: Median scored comment (applying the filter above?)
+# median = df_2300[(df_2300['score'] >= 0.75) & (df_2300['total'] > 0)].head()
+# polarity_scores(median['review'].iloc[0], False)
+# polarity_scores(median['review'].iloc[0])
+# median['review'].iloc[0]
+# # Display: Lowest scored comment (applying the filter above?)
+# lowest = df_2300[(df_2300['score'] < 0.6) & (df_2300['total'] > 0)].head()
+# polarity_scores(lowest['review'].iloc[0], False)
+# polarity_scores(lowest['review'].iloc[0])
+# lowest['review'].iloc[0]
+
+# Display a sample Pos comment
+
+# Display a sample neg comment
+
+# Display a sample Neu comment
